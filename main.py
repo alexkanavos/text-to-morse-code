3a3d20f4a8b5ea3b08be723a662ad133e880ad4f
Alex Kanavos <alexkanv@gmail.com>
2024-03-12 17:15:30 +0100

    Add a while loop for multiple conversions.

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ def text_to_morse(text: str):
 
 
 def run():
-    user_input = list(input("Text to be converted to morse code: ").upper())
-    result = text_to_morse(text=user_input)
-    print(result)
+    keep_converting = True
+    while keep_converting:
+        user_input = list(
+            input("Type the text to be converted to Morse Code: ").upper()
+        )
+        result = text_to_morse(text=user_input)
+        print(f"Morse Code: {result}")
+        if input("Do you want to convert text again [y/n]: ").lower() != "y":
+            keep_converting = False
 
 
 if __name__ == "__main__":
